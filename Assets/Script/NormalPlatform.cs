60d5e280b051ae224fccb93fa731178464376078
Gio1498 <giovanni.messina1498@gmail.com>
2021-06-19 18:38:59 +0200

    Prima prototipazione di player e piattaforme
    Aggiunto lo script PlayerMovement e NormalPlatform

---
+++ Assets/Script/NormalPlatform.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NormalPlatform : MonoBehaviour
+{
+    public Rigidbody2D PlayerRB;
+    float jumpValue = 350;
+    Vector2 jumpForce;
+
+    private void Start()
+    {
+        jumpForce = new Vector2(0, jumpValue);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.transform.tag == "Player")
+        {
+            Debug.Log("Collision");
+            PlayerRB.AddForce(jumpForce);
+        }
+    }
+}
---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float Speed;
+    Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float moveBy = x * Speed;
+        rb.velocity = new Vector2(moveBy, rb.velocity.y);
+    }
+
+    
+}
