97c2e834a25ecc52466e367645f7fd553bec0077
Gio1498 <giovanni.messina1498@gmail.com>
2021-06-23 15:23:38 +0200

    Modifiche allo script normal platform
    Aggiunto OnCollisionExit

--- Assets/Script/NormalPlatform.cs
+++ Assets/Script/NormalPlatform.cs
@@ -6,16 +6,28 @@ public class NormalPlatform : MonoBehaviour
 {
     float jumpValue = 350;
     Vector2 jumpForce;
+    private bool playerCollision;
 
     private void Start()
     {
         jumpForce = new Vector2(0, jumpValue);
+        playerCollision = false;
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.transform.tag == "Player")
+        if (collision.gameObject.CompareTag("Player") && !playerCollision)
         {
+            playerCollision = true;
             collision.rigidbody.AddForce(jumpForce);
         }
     }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+
+            playerCollision = false;
+        }
+    }
+    
 }
