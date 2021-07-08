18f6b993eb2691b4a162aecd3484550833a6e43a
Gio1498 <giovanni.messina1498@gmail.com>
2021-07-08 17:20:57 +0200

    Aggiunto l'effetto Pac-Man

---
+++ Assets/Script/PortalScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalScript : MonoBehaviour
+{
+    public Transform DestinationPortal; //Se lo script viene messo nel portale destro il destination portal sarà il portale sinistro e viceversa.
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            if (this.gameObject.name == "Right Portal")
+            {
+                collision.gameObject.transform.position = new Vector2(DestinationPortal.position.x + 0.5f, DestinationPortal.position.y);
+            }
+            if (this.gameObject.name == "Left Portal")
+            {
+                collision.gameObject.transform.position = new Vector2(DestinationPortal.position.x - 0.5f, DestinationPortal.position.y);
+            }
+        }
+    }
+}
